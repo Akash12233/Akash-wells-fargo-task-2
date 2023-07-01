f42561690b44ee3c5a22543c8f910ded94e07079
Akash Arun Tayade <akashtayade1504@gmail.com>
2023-07-01 13:08:13 +0530

    Add files via upload

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -3,6 +3,41 @@ package com.wellsfargo.counselor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // Add other instance variables with column or relationship annotations
+
+    // Default constructor
+    public MyEntity() {
+    }
+
+    // Constructor with all instance variables
+    public MyEntity(Long id /* add other parameters */) {
+        this.id = id;
+        // Initialize other instance variables
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    // Setter for other instance variables
+    // ...
+
+    // Override equals() and hashCode() methods if needed
+
+    // Other methods
+}
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Entrypoint {
